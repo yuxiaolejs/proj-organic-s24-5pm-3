27c50b463bb34682907ec6423e23d305bb3dcf05
Jenil Prajapati <jenilrajeshkumar@ucsb.edu>
2024-03-01 14:46:53 -0800

    adding repo

--- src/main/java/edu/ucsb/cs156/organic/repositories/SchoolRepository.java
+++ src/main/java/edu/ucsb/cs156/organic/repositories/SchoolRepository.java
@@ -1,20 +1,10 @@
 package edu.ucsb.cs156.organic.repositories;
 
 import edu.ucsb.cs156.organic.entities.School;
-import edu.ucsb.cs156.organic.entities.Staff;
-
-import java.util.Optional;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SchoolRepository extends CrudRepository<Staff, String> {
-    Optional<School> findByName(String name);  
-    Iterable<School> findByTermRegex(String termRegex);
-    Iterable<School> findByTermDescription(String termDescription);
-    Iterable<School> findByTermError(String termError);
-    Iterable<School> findByTermRegexAndTermDescription(String termRegex, String termDescription);
-    Iterable<School> findByTermRegexAndTermError(String termRegex, String termError);
-    Iterable<School> findByTermDescriptionAndTermError(String termDescription, String termError);
+public interface SchoolRepository extends JpaRepository<School, Long> {
+    // You can add custom query methods here if needed
 }
