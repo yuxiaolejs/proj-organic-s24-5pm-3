895736d619d556df46abfc2f6eca77fd678c4e8b
sanilkatula <124859948+sanilkatula@users.noreply.github.com>
2024-03-01 12:17:22 -0800

    sk - Mutation

--- frontend/src/main/components/Courses/CoursesTable.js
+++ frontend/src/main/components/Courses/CoursesTable.js
@@ -57,10 +57,10 @@ import React from "react";
      ];
 
      if (hasRole(currentUser, "ROLE_ADMIN") || hasRole(currentUser, "ROLE_INSTRUCTOR")) {
-         columns.push(ButtonColumn("Edit", "#4CAF50", editCallback, "CoursesTable"));
+         columns.push(ButtonColumn("Edit", "primary", editCallback, "CoursesTable"));
          columns.push(ButtonColumn("Delete", "danger", deleteCallback, "CoursesTable"));
      }
-
+     
 
 
      return <OurTable
