f9d8c0cb9fd218c934de3a596dca8a9cbf775bab
vcnardi524 <victor_c_nardi@ucsb.edu>
2024-03-06 00:35:32 -0800

    VN - implmented DELETE part of schools api/controller

--- src/main/java/edu/ucsb/cs156/organic/controllers/SchoolController.java
+++ src/main/java/edu/ucsb/cs156/organic/controllers/SchoolController.java
@@ -51,33 +51,6 @@ public class SchoolController extends ApiController{
     UserRepository userRepository;
 
 
-
-    @Operation(summary= "Create a new school")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping("/post")
-    public School postSchool(
-        @Parameter(name="abbrev") @RequestParam String abbrev,
-        @Parameter(name="name") @RequestParam String name,
-        @Parameter(name="termRegex") @RequestParam String termRegex,
-        @Parameter(name="termDescription") @RequestParam String termDescription,
-        @Parameter(name="termError") @RequestParam String termError)
-        {
-
-        School school = School.builder().build();
-        school.setAbbrev(abbrev);
-        school.setName(name);
-        school.setTermRegex(termRegex);
-        school.setTermDescription(termDescription);
-        school.setTermError(termError);
-
-        School savedSchool = schoolRepository.save(school);
-
-        return savedSchool;
-    }
-
-
-
-
     @Operation(summary= "Delete a school")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("")
@@ -88,32 +61,6 @@ public class SchoolController extends ApiController{
 
         schoolRepository.delete(school);
         return genericMessage("School with id %s deleted".formatted(abbrev));
-    }
-
-
-
-    @Operation(summary = "Update information for a school")
-    // allow for roles of ADMIN or INSTRUCTOR but only if the user is a staff member
-    // for the course
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_INSTRUCTOR')")
-    @PutMapping("/update")
-    public School updateSchool(
-            @Parameter(name = "abbrev") @RequestParam String abbrev,
-            @RequestBody @Valid School incoming) {
-
-                School school = schoolRepository.findById(abbrev)
-                .orElseThrow(() -> new EntityNotFoundException(School.class, abbrev));
-
-        school.setName(incoming.getName());
-        school.setTermRegex(incoming.getTermRegex());
-        school.setTermDescription(incoming.getTermDescription());
-        school.setTermError(incoming.getTermError());
-
-        schoolRepository.save(school);
-
-        log.info("school={}", school);
-
-        return school;
     }
 
 }
