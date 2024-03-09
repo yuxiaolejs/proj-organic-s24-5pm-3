c3e62f8b222cbf00c16bb6eacb4a7672e2bf3bfc
jenil-prajapati <124840083+jenil-prajapati@users.noreply.github.com>
2024-03-08 16:56:01 -0800

    Updated SchoolRepository.java

--- src/main/java/edu/ucsb/cs156/organic/repositories/SchoolRepository.java
+++ src/main/java/edu/ucsb/cs156/organic/repositories/SchoolRepository.java
@@ -1,10 +1,11 @@
 package edu.ucsb.cs156.organic.repositories;
 
 import edu.ucsb.cs156.organic.entities.School;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface SchoolRepository extends JpaRepository<School, Long> {
+public interface SchoolRepository extends CrudRepository<School, String> {
     // You can add custom query methods here if needed
+    // Optional<School> findById(String id);
 }
