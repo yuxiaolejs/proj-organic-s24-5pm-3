33a33546cea47c3cdd9cab79d2c759fd11ea8d7b
sanilkatula <124859948+sanilkatula@users.noreply.github.com>
2024-03-13 12:02:51 -0700

    sk - deleted commented code

--- frontend/src/main/components/Staff/StaffTable.js
+++ frontend/src/main/components/Staff/StaffTable.js
@@ -1,29 +1,10 @@
 import React from "react";
 import OurTable from "main/components/OurTable"
-//  import { useBackendMutation } from "main/utils/useBackend";
-//  import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/components/Utils/StaffUtils"
-//  import { useNavigate } from "react-router-dom";
-//  import { hasRole } from "main/utils/currentUser";
+
 
  export default function StaffTable({ staff }) {
 
-    //  const navigate = useNavigate();
-
-    //  const addCallback = (cell) => {
-    //      navigate(`/staff/edit/${cell.row.values.id}`);
-    //  };
-
-     // Stryker disable all : hard to test for query caching
-
-    //  const deleteMutation = useBackendMutation(
-    //      cellToAxiosParamsDelete,
-    //      { onSuccess: onDeleteSuccess },
-    //      ["/api/courses/getStaff"]
-    //  );
-     // Stryker restore all 
-
      // Stryker disable next-line all : TODO try to make a good test for this
-    //  const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
      const columns = [
          {
@@ -40,11 +21,6 @@ import OurTable from "main/components/OurTable"
          },
    
      ];
-
-    //  if (hasRole(currentUser, "ROLE_ADMIN") || hasRole(currentUser, "ROLE_INSTRUCTOR")) {
-    //     //  columns.push(ButtonColumn("Edit", "primary", editCallback, "CoursesTable"));
-    //     //  columns.push(ButtonColumn("Delete", "danger", deleteCallback, "StaffTable"));
-    //  }
 
      return <OurTable
          data={staff}
