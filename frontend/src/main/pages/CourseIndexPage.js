df8f08b8e4b7d5fdb0fec418f00f36f30641b63d
sanilkatula <124859948+sanilkatula@users.noreply.github.com>
2024-03-13 01:25:04 -0700

    sk - fixed the bug

--- frontend/src/main/pages/CourseIndexPage.js
+++ frontend/src/main/pages/CourseIndexPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useBackend } from 'main/utils/useBackend';
-import { hasRole } from 'main/utils/currentUser';
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+qimport BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import CoursesTable from 'main/components/Courses/CoursesTable';
 import { Button } from 'react-bootstrap';
 import { useCurrentUser, hasRole} from 'main/utils/currentUser';
