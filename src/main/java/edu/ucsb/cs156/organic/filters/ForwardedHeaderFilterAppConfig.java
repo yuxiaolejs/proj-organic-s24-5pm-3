06a95c35b35fcc8cd824c7a717fff3fa2b6d43ea
Tianle Yu <yulele28@126.com>
2024-05-16 19:22:27 -0700

    [feat] changed filters to extend

--- src/main/java/edu/ucsb/cs156/organic/filters/ForwardedHeaderFilterAppConfig.java
+++ src/main/java/edu/ucsb/cs156/organic/filters/ForwardedHeaderFilterAppConfig.java
@@ -4,10 +4,11 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
-public class ForwardedHeaderFilterAppConfig {
+public class ForwardedHeaderFilterAppConfig extends WebSecurityConfigurerAdapter{
 
    @Bean // https://stackoverflow.com/questions/51404552/spring-boot-oauth-always-redirecting-to-http-ibm-cloud-cf-spring-boot-2/51500554#51500554
    FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
